d2cbc13c7e5a1aeccf2e9d0b85f909a05ba4b036
DanielTuttos <48894430+DanielTuttos@users.noreply.github.com>
2020-07-02 16:06:30 -0500

    Se agrego formas rapidas de crear registros aleatorios

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,14 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA2KZ4xvOaD832olsC1BG_IWhrmEibRSEA",
-    authDomain: "practicafirebasesql.firebaseapp.com",
-    databaseURL: "https://practicafirebasesql.firebaseio.com",
-    projectId: "practicafirebasesql",
-    storageBucket: "practicafirebasesql.appspot.com",
-    messagingSenderId: "801828093731",
-    appId: "1:801828093731:web:e231bd9d13c011f2ba5696",
-    measurementId: "G-R09X8H1L6S"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,39 @@
 import db from './firebase/config';
 import { retornaDocumentos } from './helpers/mostrarDocumentos';
 
+const faker = require('faker');
+
+faker.locale = "es";
+
 const usuariosRef = db.collection('usuarios');
 
-usuariosRef.orderBy('nombre')
-    .get().then(retornaDocumentos);
+function curpRandom(limit = 10) {
+    var result = '';
+    var characters = '0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < limit; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+const registrarUsuarios = () => {
+    for (let i = 0; i <= 3; i++) {
+        const usuario = {
+            curp: curpRandom(),
+            date: Date.now(),
+            lastName: faker.name.lastName(),
+            name: faker.name.firstName(),
+            rango: 'Directivo',
+            escuela: {
+                id: '43YffzSHtJZP10eVWh3t'
+            }
+        }
+
+        console.log(usuario);
+        usuariosRef
+            .add(usuario)
+            .then(docRef => console.log('creado'))
+            .catch(e => console.error(e));
+    }
+}
